8d1cf7f81c4f9f2a892e38f2a5d1d908e385b19b
TolMen Jy <cotton.frachisse@gmail.com>
2025-09-09 14:38:34 +0200

    Ajout V1 Navbar (Modification plus tard)

---
+++ private/src/view/component/navbar.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
+    <div class="container">
+        <a class="navbar-brand" href="index.php?page=annonces">Petites annonces</a>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="index.php?page=annonces">Annonces</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.php?page=connexion">Connexion</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.php?page=inscription">Inscription</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
