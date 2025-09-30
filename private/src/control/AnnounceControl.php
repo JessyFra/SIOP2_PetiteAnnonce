f0b234810400e1d0fb7b9d7b9184c6899d8c5815
nolansio <nolanarsac105@gmail.com>
2025-09-30 14:12:52 +0200

    Correction de variable non traduite

--- private/src/control/AnnounceControl.php
+++ private/src/control/AnnounceControl.php
@@ -11,25 +11,25 @@ class AnnounceControl {
         $categories = CategoryDAO::getAll();
 
         foreach ($categories as $category) {
-            $annonces = $category->getAnnounces();
+            $announces = $category->getAnnounces();
 
             echo "<section class='category'>";
                 echo "<h3>".$category->getName()."</h3>";
                 echo "<div class='articles'>";
 
-                foreach ($annonces as $annonce) {
+                foreach ($announces as $announce) {
                         echo "<article class='announce'>";
                             echo "<div class='announce-picture'>";
                                 echo "<img class='picture' 
-                                    src='public/assets/img/".$annonce->getId().".png' 
-                                    alt='".$annonce->getTitle()."' 
+                                    src='public/assets/img/".$announce->getId().".png' 
+                                    alt='".$announce->getTitle()."' 
                                     onerror=\"this.onerror=null;this.src='public/assets/default.png'\">";
                             echo "</div>";
                                 echo "<div class='announce-description'>";
-                                echo "<h3>".$annonce->getTitle()."</h3>";
-                                echo "<p>".$annonce->getDescription()."</p>";
-                                echo "<p> Prix : ".$annonce->getPrice()." €</p>";
-                                echo "<p> 📍 ".$annonce->getCityName()."</p>";
+                                echo "<h3>".$announce->getTitle()."</h3>";
+                                echo "<p>".$announce->getDescription()."</p>";
+                                echo "<p> Prix : ".$announce->getPrice()." €</p>";
+                                echo "<p> 📍 ".$announce->getCityName()."</p>";
                                 echo "</div>";
                         echo "</article>";
                 }
