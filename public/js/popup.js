10afb24fb82a1b10a047812ab8072a3dde20a28c
TolMen Jy <cotton.frachisse@gmail.com>
2025-09-30 16:15:35 +0200

    Fonctionnement de la popup

---
+++ public/js/popup.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const box = document.getElementById("messageBox");
+    if (box) {
+        const msg = box.dataset.message;
+
+        // Détection du type de message
+        let type = "error";
+        if (
+            msg.includes("réussie") ||
+            msg.includes("connecter") ||
+            msg.includes("succès")
+        ) {
+            type = "success";
+        }
+
+        const popup = document.createElement("div");
+        popup.className = `popup-message ${type}`;
+        popup.textContent = msg;
+
+        document.body.appendChild(popup);
+
+        // Suppression automatique après animation
+        setTimeout(() => {
+            popup.remove();
+        }, 5000);
+    }
+});
