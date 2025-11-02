684ddcca5a52466cb39c1fe7fe7cd7287f4edd0a
TolMen Jy <cotton.frachisse@gmail.com>
2025-11-02 14:24:39 +0100

    Ajout champ bannissement

--- private/src/model/DTO/UserDTO.php
+++ private/src/model/DTO/UserDTO.php
@@ -1,6 +1,7 @@
 <?php
 
-class UserDTO {
+class UserDTO
+{
 
     private $id;
     private $name;
@@ -9,8 +10,10 @@ class UserDTO {
     private $biography;
     private $role;
     private $created_at;
+    private $banned;
 
-    public function __construct($id, $name, $hashed_password, $global_name, $biography, $role, $created_at) {
+    public function __construct($id, $name, $hashed_password, $global_name, $biography, $role, $created_at, $banned = 0)
+    {
         $this->id = $id;
         $this->name = $name;
         $this->hashed_password = $hashed_password;
@@ -18,58 +21,90 @@ class UserDTO {
         $this->biography = $biography;
         $this->role = $role;
         $this->created_at = $created_at;
+        $this->banned = $banned;
     }
 
     // Getters et Setters
-    public function getId() {
-		return $this->id;
-	}
-	public function setId($id) {
-		$this->id = $id;
-	}
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
 
-    public function getName() {
+    public function getName()
+    {
         return $this->name;
     }
-    public function setName($name) {
+    public function setName($name)
+    {
         $this->name = $name;
     }
 
-    public function getHashedPassword() {
+    public function getHashedPassword()
+    {
         return $this->hashed_password;
     }
-    public function setHashedPassword($hashed_password) {
+    public function setHashedPassword($hashed_password)
+    {
         $this->hashed_password = $hashed_password;
     }
 
-    public function getGlobalName() {
+    public function getGlobalName()
+    {
         return $this->global_name;
     }
-    public function setGlobalName($global_name) {
+    public function setGlobalName($global_name)
+    {
         $this->global_name = $global_name;
     }
 
-    public function getBiography() {
+    public function getBiography()
+    {
         return $this->biography;
     }
-    public function setBiography($biography) {
+    public function setBiography($biography)
+    {
         $this->biography = $biography;
     }
 
-    public function getRole() {
+    public function getRole()
+    {
         return $this->role;
     }
-    public function setRole($role) {
+    public function setRole($role)
+    {
         $this->role = $role;
     }
 
-    public function getCreatedAt() {
+    public function getCreatedAt()
+    {
         return $this->created_at;
     }
-    public function setCreatedAt($created_at) {
+    public function setCreatedAt($created_at)
+    {
         $this->created_at = $created_at;
     }
 
-    // Autres fonctions
+    public function getBanned()
+    {
+        return $this->banned;
+    }
+    public function setBanned($banned)
+    {
+        $this->banned = $banned;
+    }
 
+    // Autres fonctions
+    public function isBanned()
+    {
+        return $this->banned == 1;
+    }
+
+    public function isAdmin()
+    {
+        return $this->role === 'admin';
+    }
 }
