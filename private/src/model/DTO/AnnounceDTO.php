f2076fe6ad38c14a2ea64aedb2e56fa66f03e671
TolMen Jy <cotton.frachisse@gmail.com>
2025-11-02 14:23:40 +0100

    Ajout champ Type (offre/demande)

--- private/src/model/DTO/AnnounceDTO.php
+++ private/src/model/DTO/AnnounceDTO.php
@@ -3,93 +3,123 @@
 include_once 'private/src/model/DAO/CityDAO.php';
 include_once 'private/src/model/DAO/UserDAO.php';
 
-class AnnounceDTO {
+class AnnounceDTO
+{
 
     private $id;
     private $title;
     private $description;
     private $price;
     private $status;
+    private $type;
     private $city_id;
     private $author_id;
     private $created_at;
 
-    public function __construct($id, $title, $description, $price, $status, $city_id, $author_id, $created_at) {
+    public function __construct($id, $title, $description, $price, $status, $type, $city_id, $author_id, $created_at)
+    {
         $this->id = $id;
         $this->title = $title;
         $this->description = $description;
         $this->price = $price;
         $this->status = $status;
+        $this->type = $type;
         $this->city_id = $city_id;
         $this->author_id = $author_id;
         $this->created_at = $created_at;
     }
 
     // Getters et Setters
-    public function getId() {
+    public function getId()
+    {
         return $this->id;
     }
-    public function setId($id) {
+    public function setId($id)
+    {
         $this->id = $id;
     }
 
-    public function getTitle() {
+    public function getTitle()
+    {
         return $this->title;
     }
-    public function setTitle($title) {
+    public function setTitle($title)
+    {
         $this->title = $title;
     }
 
-    public function getDescription() {
+    public function getDescription()
+    {
         return $this->description;
     }
-    public function setDescription($description) {
+    public function setDescription($description)
+    {
         $this->description = $description;
     }
 
-    public function getPrice() {
+    public function getPrice()
+    {
         return $this->price;
     }
-    public function setPrice($price) {
+    public function setPrice($price)
+    {
         $this->price = $price;
     }
 
-    public function getStatus() {
+    public function getStatus()
+    {
         return $this->status;
     }
-
-    public function setStatus($status) {
+    public function setStatus($status)
+    {
         $this->status = $status;
     }
 
-    public function getCityId() {
+    public function getType()
+    {
+        return $this->type;
+    }
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    public function getCityId()
+    {
         return $this->city_id;
     }
-    public function setCityId($city_id) {
+    public function setCityId($city_id)
+    {
         $this->city_id = $city_id;
     }
 
-    public function getAuthorId() {
+    public function getAuthorId()
+    {
         return $this->author_id;
     }
-    public function setAuthorId($author_id) {
+    public function setAuthorId($author_id)
+    {
         $this->author_id = $author_id;
     }
 
-    public function getCreatedAt() {
+    public function getCreatedAt()
+    {
         return $this->created_at;
     }
-    public function setCreatedAt($created_at) {
+    public function setCreatedAt($created_at)
+    {
         $this->created_at = $created_at;
     }
 
     // Autres fonctions
-    public function getCityName() {
+    public function getCityName()
+    {
         $city = CityDAO::get($this->city_id);
         return $city->getName();
     }
 
-    public function getAuthorName() {
+    public function getAuthorName()
+    {
         $author = UserDAO::get($this->author_id);
         return $author->getName();
     }
