4e48a1b8c3a60b25aefc8b6b6c9a7d626caea499
nolansio <nolanarsac105@gmail.com>
2025-11-04 16:31:48 +0100

    Déplacement de l'envoi du message dans une fonction

--- public/js/inboxScript.js
+++ public/js/inboxScript.js
@@ -1,17 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    scrollToBottom();
-
     window.addEventListener("load", () => {
         scrollToBottom();
     });
 
     const sendMessageButton = document.getElementById("sendMessageButton");
     const messageTextarea = document.getElementById("messageTextarea");
-    const messagesBox = document.getElementById("messagesBox");
 
     sendMessageButton.addEventListener("click", function () {
-        scrollToBottom();
-
         const content = messageTextarea.value;
         const recipientId = new URLSearchParams(window.location.search).get("id");
 
@@ -19,41 +14,50 @@ document.addEventListener("DOMContentLoaded", function () {
             return;
         }
 
-        let message = {};
-        message.content = content;
-        message.receiverId = recipientId;
-
-        const ajaxRequest = new XMLHttpRequest();
-        ajaxRequest.open("POST", "index.php?page=sendMessageAjax");
-        ajaxRequest.setRequestHeader("Content-Type", "application/json");
-        ajaxRequest.send(JSON.stringify(message));
-
-        ajaxRequest.onreadystatechange = function() {
-            if (ajaxRequest.readyState === 4) {
-                if (ajaxRequest.status === 200) {
-                    messageTextarea.value = "";
-
-                    let messageBox = document.createElement("section");
-                    messageBox.classList.add("messageBox");
-                    messageBox.classList.add("mbox-left");
-
-                    let message = document.createElement("article");
-                    message.classList.add("message");
-                    message.innerHTML = content;
-
-                    messageBox.appendChild(message);
-                    messagesBox.appendChild(messageBox);
-                }
-
-                console.log(ajaxRequest.status);
-                scrollToBottom();
-            }
-        };
+        sendMessage(content, recipientId);
     });
+
 });
 
 
 function scrollToBottom() {
     const messagesBox = document.getElementById("messagesBox");
     messagesBox.scrollTop = messagesBox.scrollHeight;
+}
+
+
+function sendMessage(content, receiverId) {
+    let message = {};
+    message.content = content;
+    message.receiverId = receiverId;
+
+    const ajaxRequest = new XMLHttpRequest();
+    ajaxRequest.open("POST", "index.php?page=sendMessageAjax");
+    ajaxRequest.setRequestHeader("Content-Type", "application/json");
+    ajaxRequest.send(JSON.stringify(message));
+
+    ajaxRequest.onreadystatechange = function() {
+        if (ajaxRequest.readyState === 4) {
+            if (ajaxRequest.status === 200) {
+                const messageTextarea = document.getElementById("messageTextarea");
+                const messagesBox = document.getElementById("messagesBox");
+
+                messageTextarea.value = "";
+
+                let messageBox = document.createElement("section");
+                messageBox.classList.add("messageBox");
+                messageBox.classList.add("mbox-left");
+
+                let message = document.createElement("article");
+                message.classList.add("message");
+                message.innerHTML = content;
+
+                messageBox.appendChild(message);
+                messagesBox.appendChild(messageBox);
+            }
+
+            console.log(ajaxRequest.status);
+            scrollToBottom();
+        }
+    };
 }
