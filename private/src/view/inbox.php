4437b4a8d8a936426676614aecfcd7adcf49b654
nolansio <nolanarsac105@gmail.com>
2025-11-07 10:59:20 +0100

    Modification minime du CSS de Inbox

--- private/src/view/inbox.php
+++ private/src/view/inbox.php
@@ -18,22 +18,18 @@ $messages = MessageDAO::getAll($meId, $recipientId);
 
 <section id="mainBox">
     <div id="messagesBox" class="d-flex w-100 h-100" data-me-id="<?php echo htmlspecialchars($meId, ENT_QUOTES); ?>" data-recipient-id="<?php echo htmlspecialchars($recipientId, ENT_QUOTES); ?>">
-        <?php
+        <?php foreach ($messages as $message) {
+            $classes = $message->getAuthorId() == $meId ? "messageBox mbox-right" : "messageBox mbox-left";
+            $msgClass = $message->getAuthorId() == $meId ? "message msg-right" : "message msg-left";
 
-        foreach ($messages as $message) {
-            if ($message->getAuthorId() == $meId) {
-                echo "<section class='messageBox mbox-right'>";
-            } else {
-                echo "<section class='messageBox mbox-left'>";
-            }
-
-        ?>
-            <article class="message"><?php echo htmlspecialchars($message->getContent(), ENT_QUOTES); ?></article></section>
-        <?php } ?>
+            echo "<section class='$classes'><article class='$msgClass'>" .
+                    htmlspecialchars($message->getContent(), ENT_QUOTES) .
+                    "</article></section>";
+        } ?>
     </div>
 
     <div class="d-flex w-100">
         <textarea id="messageTextarea" class="form-control" placeholder="Envoyer un message"></textarea>
-        <button id="sendMessageButton" class="btn btn-primary">Envoyer</button>
+        <button id="sendMessageButton" class="btn">Envoyer</button>
     </div>
 </section>
